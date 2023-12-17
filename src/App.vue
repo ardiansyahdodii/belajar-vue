b9f97602088833dbde0aa9d3be6ae38c92aa259b
ardiansyahdodii <dodyn890@gmail.com>
2023-12-18 00:34:04 +0700

    component, props, emits

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <Student @response="showData" studentName="Dodi" />
+    <Student @response="(select) => selectedStudent = select" studentName="Ardiansyah" />
+
+    <div>
+      Student dipilih : {{ selectedStudent }}
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import Student from './components/Student.vue';
+export default {
+  components: {
+    Student
+  },
+  data() {
+    return {
+      selectedStudent: '',
+    }
+  },
+  methods: {
+    showData(select) {
+      this.selectedStudent = select
+    }
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
+</script>
+
+<!-- <style scoped>
+</style> -->
---
+++ src/components/Student.vue
@@ -0,0 +1,24 @@
+
+<template>
+    <div>
+        Ini adalah data dari siswa bernama : {{ studentName }}
+        <button @click="showData">Show Detail</button>
+    </div>
+</template>
+
+<script>
+export default{
+    props: {
+        studentName: String,
+    },
+    emits: ['response'],
+    methods: {
+        showData(){
+            this.$emit('response', this.studentName)
+        }
+    }
+}
+</script>
+
+// props => data dari parent ke child
+// emits => data dari child ke parent
